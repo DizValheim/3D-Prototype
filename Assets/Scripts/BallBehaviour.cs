4b2b31232e11ec1edd6b83f55b98c0fcff687247
DizValheim <122073923+DizValheim@users.noreply.github.com>
2023-10-20 21:28:48 +0530

    Basic Presentation + Demo Scripts

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -1,48 +1,63 @@
+//PLEASE USE THIS AS EXAMPLE TEMPLATE FOR OUR FUTURE SCRIPTS. 
+//EVERYTHING SHOULD BE UNDER "Ariston" NAMESPACE. 
+//LATER SUB-NAMESPACES WOULD BE ADDED LIKE "Ariston.InteractableObjects".
 using UnityEngine;
 
-public class BallBehaviour : MonoBehaviour
+namespace Ariston
 {
-    [SerializeField]GameObject ball;
-    [SerializeField]float upwardForce=-15; //Always write f after float variables
-    [SerializeField]float forwardForce=20;  
-    
-    private Vector3 screenPosition;
-    private Vector3 worldPosition;
-    private Vector3 spawnPoint;
-    private Rigidbody rb;
-    
-    private Camera mainCamera;  //I Changed it to mainCamera from MainCamera
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class BallBehaviour : MonoBehaviour
     {
-        mainCamera = Camera.main;   //You can use Camera.main instead GetComponent (GetComponent allocates memory even if no element is found) 
-                                    //to get the reference to main camera. It's preferable since it's predefined and has less chance of errors...
-    }
+        #region Variables
+        [SerializeField] private GameObject ball;
+        [SerializeField] private float forceMagnitude = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Code for mouse position
-        screenPosition=Input.mousePosition;
-        screenPosition.z=Camera.main.nearClipPlane +1;
-        worldPosition=Camera.main.ScreenToWorldPoint(screenPosition);
-        spawnPoint=mainCamera.transform.position;
-        spawnPoint.z+=2;
+        private float despawnTime = 4f; //I made this a variable for easier modification while testing (this can be serialized if required)
+        private Rigidbody rb;
+        
+        private Camera mainCamera;
 
+        #endregion 
 
-        //Code for ball initialization and destruction
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        #region Unity Functions
+        
+        // Start is called before the first frame update
+        void Start()
         {
-            GameObject SpawnedBall = Instantiate(ball,spawnPoint,Quaternion.identity);  //The ball should be named spawnedBall (Naming rule violation)
-            rb=SpawnedBall.GetComponent<Rigidbody>();
-            rb.AddForce(transform.up*upwardForce,ForceMode.Impulse);    //Upward Impulse
-            rb.AddForce(worldPosition*forwardForce,ForceMode.Impulse);  //Forward Impulse
-            Destroy(SpawnedBall,4f);        //Destroy ball entity
+            mainCamera = Camera.main;   //You can use Camera.main instead GetComponent (GetComponent allocates memory even if no element is found) 
+                                        //to get the reference to main camera. It's preferable since it's predefined and has less chance of errors...
         }
 
-        //Also a suggestion: Try to give space in between variables and operators (It'll be a lot more readable for someone else reading your code)
+        // Update is called once per frame
+        void Update()
+        {
 
-    }    
+            if (Input.GetMouseButtonDown(0))
+            {
+                // RaycastHit hit;      // We can also inline this declaration inside the if condition like done below!!
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); //Ray is basically what it sounds like.. and I hope you understand rest of this line 
+                                                                            //since the name of the methods kind of justify it
+
+                if (Physics.Raycast(ray, out RaycastHit hit))       //Here you can see I declared RaycastHit inside the if condition after out keyword 
+                                                                    //(Which makes the variable accessible even outside the if statement. It'll be used 
+                                                                    //often with raycasts since you need the hit point)  
+                {
+
+                    GameObject spawnedObject = Instantiate(ball, mainCamera.transform.position, Quaternion.identity);
+
+                    // Rigidbody rb = spawnedObject.GetComponent<Rigidbody>();      //Due to the memory allocation problem here I used TryGetComponent instead of GetComponent
+                    if (spawnedObject.TryGetComponent<Rigidbody>(out rb))           //So here we can again see the use of out keyword to get the reference 
+                                                                                    //out of the method (I'm assuming you understand what the out keyword 
+                                                                                    //does by now.. still if there's any doubt ask in the appropriate channels or during meetings)
+                    {
+                        Vector3 forceDirection = (hit.point - mainCamera.transform.position).normalized;
+                        rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+                    }
+                    Destroy(spawnedObject,despawnTime);
+                }
+            }
+        }
+
+        #endregion
+    }
+
 }
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,30 +1,16 @@
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
+namespace Ariston
 {
-    [SerializeField] float speed = 0f;
-    Vector3 ForceToAdd = new Vector3(); //You can use new() (but I think you already know ;))
-    Camera MainCamera = new Camera();   //This is a major blunder!! Camera is an ingame object so it shouldn't be instantiated like this
-                                        //but I leave the changes to you so that it'll be easier to remember!!
-                                        //Because all ingame objects are instantiated using Instantiate() method!!
-                                        //You can just declare a var of type camera like this: Camera mainCamera; (Remember this doesn't point 
-                                        //anywhere(and it shouldn't) until you get the reference to camera in the start method)
-                                        //Also rename the variables according to naming conventions :)
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class CameraMovement : MonoBehaviour
     {
-        ForceToAdd = new Vector3(0, 0, speed * Time.deltaTime);
-        MainCamera = GetComponent<Camera>();      //This can be fixed also!! but I leave that to you for practice :)
-    }
+        [SerializeField]float moveSpeed = 0f;
+        private Vector3 moveDirection = Vector3.forward;
 
-    // Update is called once per frame
-    void Update()
-    {
-        MainCamera.transform.position+=ForceToAdd;  //here you are actually adding values to the camera's position 
-                                                    //Which technically isn't "force" therefore the variable should be named something like posToAdd but it's totally upto you
+        void Update()
+        {
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
     }
 }
 
-//BUT OVERALL THESE ARE PRETTY WELL WRITTEN CODE COMPARED TO MY FIRST UNITY SCRIPTS SO DON'T THINK THAT I'M PICKING OUT MISTAKES (I'M JUST SUGGESTING A BETTER WAY :))
