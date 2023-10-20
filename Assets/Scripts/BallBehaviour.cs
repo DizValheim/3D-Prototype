b57e8ffc5bf06e8d25b9df3f3b6c1f29418091f7
DizValheim <122073923+DizValheim@users.noreply.github.com>
2023-10-20 20:03:01 +0530

    Add some suggestions for Abhishek

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
     [SerializeField]GameObject ball;
-    [SerializeField]float upwardForce=-15;
-    [SerializeField]float forwardForce=20;
+    [SerializeField]float upwardForce=-15; //Always write f after float variables
+    [SerializeField]float forwardForce=20;  
+    
     private Vector3 screenPosition;
     private Vector3 worldPosition;
     private Vector3 spawnPoint;
     private Rigidbody rb;
     
-    private Camera MainCamera;
+    private Camera mainCamera;  //I Changed it to mainCamera from MainCamera
+
+
     // Start is called before the first frame update
     void Start()
     {
-        MainCamera=GetComponent<Camera>();
+        mainCamera = Camera.main;   //You can use Camera.main instead GetComponent (GetComponent allocates memory even if no element is found) 
+                                    //to get the reference to main camera. It's preferable since it's predefined and has less chance of errors...
     }
 
     // Update is called once per frame
@@ -24,18 +28,21 @@ public class BallBehaviour : MonoBehaviour
         screenPosition=Input.mousePosition;
         screenPosition.z=Camera.main.nearClipPlane +1;
         worldPosition=Camera.main.ScreenToWorldPoint(screenPosition);
-        spawnPoint=MainCamera.transform.position;
+        spawnPoint=mainCamera.transform.position;
         spawnPoint.z+=2;
 
 
         //Code for ball initialization and destruction
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            GameObject SpawnedBall=Instantiate(ball,spawnPoint,Quaternion.identity);
+            GameObject SpawnedBall = Instantiate(ball,spawnPoint,Quaternion.identity);  //The ball should be named spawnedBall (Naming rule violation)
             rb=SpawnedBall.GetComponent<Rigidbody>();
             rb.AddForce(transform.up*upwardForce,ForceMode.Impulse);    //Upward Impulse
             rb.AddForce(worldPosition*forwardForce,ForceMode.Impulse);  //Forward Impulse
             Destroy(SpawnedBall,4f);        //Destroy ball entity
         }
-    }
+
+        //Also a suggestion: Try to give space in between variables and operators (It'll be a lot more readable for someone else reading your code)
+
+    }    
 }
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,19 +2,29 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField]float speed=0f;
-    Vector3 ForceToAdd=new Vector3();
-    Camera MainCamera=new Camera();
+    [SerializeField] float speed = 0f;
+    Vector3 ForceToAdd = new Vector3(); //You can use new() (but I think you already know ;))
+    Camera MainCamera = new Camera();   //This is a major blunder!! Camera is an ingame object so it shouldn't be instantiated like this
+                                        //but I leave the changes to you so that it'll be easier to remember!!
+                                        //Because all ingame objects are instantiated using Instantiate() method!!
+                                        //You can just declare a var of type camera like this: Camera mainCamera; (Remember this doesn't point 
+                                        //anywhere(and it shouldn't) until you get the reference to camera in the start method)
+                                        //Also rename the variables according to naming conventions :)
+
+
     // Start is called before the first frame update
     void Start()
     {
-        ForceToAdd=new Vector3(0,0,speed*Time.deltaTime);
-        MainCamera=GetComponent<Camera>();
+        ForceToAdd = new Vector3(0, 0, speed * Time.deltaTime);
+        MainCamera = GetComponent<Camera>();      //This can be fixed also!! but I leave that to you for practice :)
     }
 
     // Update is called once per frame
     void Update()
     {
-        MainCamera.transform.position+=ForceToAdd;
+        MainCamera.transform.position+=ForceToAdd;  //here you are actually adding values to the camera's position 
+                                                    //Which technically isn't "force" therefore the variable should be named something like posToAdd but it's totally upto you
     }
 }
+
+//BUT OVERALL THESE ARE PRETTY WELL WRITTEN CODE COMPARED TO MY FIRST UNITY SCRIPTS SO DON'T THINK THAT I'M PICKING OUT MISTAKES (I'M JUST SUGGESTING A BETTER WAY :))
